95ec0a3e03e7e0406341a7f4ce10d46602e50627
bensonlu518 <do860518@gmail.com>
2022-01-22 22:08:35 +0800

    test

--- telebot.py
+++ telebot.py
@@ -98,7 +98,7 @@ while 1==1:
     for symbol in asset:
 
         try:
-            bot.sendMessage(receiver_id, symbol+' 1 hour change\n'+price_alter(symbol,start,end,timeframe,change)) # send a activation message to telegram receiver id
+            #bot.sendMessage(receiver_id, symbol+' 1 hour change\n'+price_alter(symbol,start,end,timeframe,change)) # send a activation message to telegram receiver id
 
         except Exception:
             #bot.sendMessage(receiver_id, 'error: '+symbol) # send a activation message to telegram receiver id
